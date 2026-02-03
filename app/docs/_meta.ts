14ef5a5d1f52b364d8bc59980b7eb2a5f21fd3dc
DennisonBertram <dennison+github@dennisonbertram.com>
2026-02-02 19:51:53 -0500

    curator: implement fixes and add docs

--- app/docs/_meta.ts
+++ app/docs/_meta.ts
@@ -1,6 +1,7 @@
 export default {
   index: 'Start Here',
   'getting-started': 'Getting Started',
+  curator: 'Curator',
   api: 'API Reference',
   contracts: 'Smart Contracts'
 }
---
+++ app/docs/curator/_meta.ts
@@ -0,0 +1,10 @@
+export default {
+  index: 'Overview',
+  'how-it-works': 'How It Works',
+  personas: 'Personas',
+  configuration: 'Configuration',
+  deployment: 'Deployment',
+  operations: 'Operations',
+  security: 'Security',
+  development: 'Development'
+}
